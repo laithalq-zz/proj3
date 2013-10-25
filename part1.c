7d3d66002981d7c1fa3fc1a2e17ea2b582a2d85a
Laith Alqaisi <laithalq@berkeley.edu>
2013-10-25 12:05:58 -0700

    Intrinsics Guide added.

--- part1.c
+++ part1.c
@@ -4,6 +4,7 @@
 int conv2D(float* in, float* out, int data_size_X, int data_size_Y,
                     float* kernel)
 {
+     
     // the x coordinate of the kernel's center
     int kern_cent_X = (KERNX - 1)/2;
     // the y coordinate of the kernel's center
@@ -11,7 +12,7 @@ int conv2D(float* in, float* out, int data_size_X, int data_size_Y,
     
     // main convolution loop
 	for(int x = 0; x < data_size_X; x++){ // the x coordinate of the output location we're focusing on
-		for(int y = 0; y < data_size_Y; y++){ // the y coordinate of theoutput location we're focusing on
+		for(int y = 0; y < data_size_Y; y++){ // the y coordinate of the output location we're focusing on
 			for(int i = -kern_cent_X; i <= kern_cent_X; i++){ // kernel unflipped x coordinate
 				for(int j = -kern_cent_Y; j <= kern_cent_Y; j++){ // kernel unflipped y coordinate
 					// only do the operation if not out of bounds
