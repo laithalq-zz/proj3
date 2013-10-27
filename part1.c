48f58219782a7cc7998289b3baf0f12ac458d6e8
Laith Alqaisi <laithalq@berkeley.edu>
2013-10-27 16:00:22 -0700

    Math Voodoo Shit, No SSE on this one --Avg 9.9 Gflop/s

--- part1.c
+++ part1.c
@@ -11,120 +11,42 @@ int conv2D(float* in, float* out, int data_size_X, int data_size_Y,
     // the y coordinate of the kernel's center
     int kern_cent_Y = (KERNY - 1)/2;
 	
-	int pad_amount_X = kern_cent_X+1, pad_amount_Y = kern_cent_Y;
-    int padded_size_X = data_size_X + pad_amount_X*2, padded_size_Y = data_size_Y + pad_amount_Y*2;
+    int padded_size_X = data_size_X + 2, padded_size_Y = data_size_Y + 2;
+	int padded_size = padded_size_X * padded_size_Y;
 	float* padded_in = calloc(padded_size_X*padded_size_Y, sizeof(float));
-
-	for(int y = pad_amount_Y; y < pad_amount_Y + data_size_Y; y++){
-		for(int x = 0; x < data_size_X; x++)
-			padded_in[y*padded_size_X + x + pad_amount_X] = in[(y-pad_amount_Y) * data_size_X + x];
+	
+	float flipped_kernel[9];
+	int pad_index = 1+padded_size_X, in_index = 0, out_index = 0;
+	int last_in_row;
+	int last_in_padded = pad_index + padded_size - padded_size_X - padded_size_X - 2; 
+	for(; pad_index < last_in_padded; pad_index+=2){
+		last_in_row = pad_index + data_size_X;
+		for(; pad_index < last_in_row; pad_index++)
+			padded_in[pad_index] = in[in_index++];
 	}
    	//=================Kernel Flip=========================== 
-	int padded_kernel_size = KERNY * (KERNX + 1);
-	float* padded_flipped_kernel = calloc(padded_kernel_size, sizeof(float));
-    for(int row = 0; row < KERNY; row++){
-    	for(int col = 0; col < KERNX; col++)
-			padded_flipped_kernel[row*(KERNX+1) + col] = kernel[row*KERNX + col];
+    for(int i = 0,j=8; i < 9; i++,j--){
+		flipped_kernel[i] = kernel[j];
     }
-    __m128 kernel_row1 = _mm_loadu_ps(padded_flipped_kernel);
-    __m128 kernel_row2 = _mm_loadu_ps(padded_flipped_kernel+KERNX+1);
-    __m128 kernel_row3 = _mm_loadu_ps(padded_flipped_kernel+2*(KERNX+1));
-
-	__m128 matrix_row = _mm_setzero_ps();
 	
-	// Result registers for storing results of dot products
-	__m128 res_row = _mm_setzero_ps();
-	__m128 sum_res1 = _mm_setzero_ps();
-	__m128 sum_res2 = _mm_setzero_ps();
-	__m128 sum_res3 = _mm_setzero_ps();	
-	const int dp_mask = 0b01110111;
-	float* sum = malloc(4 * sizeof(float));
-	float* in_location = padded_in + pad_amount_X - 1 + (pad_amount_Y -1 )* padded_size_X;
-	int out_location = 0; 
-	int x = 0;
-	int y = 0;
-	//=================Main Convolution Loop=================
-    for(; y < data_size_Y; y++){ // the y coordinate of the output location we're focusing on
-    	for(; x < data_size_X-2; x+=3){ // the x coordinate of the output location we're focusing on
-			in_location = padded_in + pad_amount_X + x - 1 + (pad_amount_Y + y - 1) * padded_size_X;
-			matrix_row = _mm_loadu_ps(in_location);
-			res_row = _mm_dp_ps(matrix_row, kernel_row1, dp_mask);
-			sum_res1 = res_row;
-		
-			in_location++;	
-    		matrix_row = _mm_loadu_ps(in_location);
-			res_row = _mm_dp_ps(matrix_row, kernel_row1, dp_mask);
-			sum_res2 = res_row;
-			
-			in_location++;	
-			matrix_row = _mm_loadu_ps(in_location);
-			res_row = _mm_dp_ps(matrix_row, kernel_row1, dp_mask);
-			sum_res3 = res_row;
-
-			in_location -= 2;
-			in_location+= padded_size_X; 
-
-			matrix_row = _mm_loadu_ps(in_location);
-			res_row = _mm_dp_ps(matrix_row, kernel_row2, dp_mask);
-			sum_res1 = _mm_add_ps(res_row, sum_res1);
-
-			in_location++;
-			matrix_row = _mm_loadu_ps(in_location);
-			res_row = _mm_dp_ps(matrix_row, kernel_row2, dp_mask);
-			sum_res2 = _mm_add_ps(res_row, sum_res2);
-
-			in_location++;
-			matrix_row = _mm_loadu_ps(in_location);
-			res_row = _mm_dp_ps(matrix_row, kernel_row2, dp_mask);
-			sum_res3 = _mm_add_ps(res_row, sum_res3);
-		
-			in_location -= 2;
-			in_location+= padded_size_X;			
-	
-			matrix_row = _mm_loadu_ps(in_location);
-			res_row = _mm_dp_ps(matrix_row, kernel_row3, dp_mask);
-			sum_res1 = _mm_add_ps(res_row, sum_res1);
-	
-			in_location++;
-			matrix_row = _mm_loadu_ps(in_location);
-			res_row = _mm_dp_ps(matrix_row, kernel_row3, dp_mask);
-			sum_res2 = _mm_add_ps(res_row, sum_res2);
-		
-			in_location++;
-			matrix_row = _mm_loadu_ps(in_location);
-			res_row = _mm_dp_ps(matrix_row, kernel_row3, dp_mask);
-			sum_res3 = _mm_add_ps(res_row, sum_res3);
-
-			_mm_storeu_ps(sum, sum_res1);
-			out[out_location++] = sum[0];
-
-			_mm_storeu_ps(sum, sum_res2);
-			out[out_location++] = sum[0];
-    		
-			_mm_storeu_ps(sum, sum_res3);
-			out[out_location++] = sum[0];	
+	out_index = 0;
+	//pad_index = 1 + padded_size_X;
+	for(pad_index = 1 + padded_size_X; pad_index < last_in_padded; pad_index+=2){ // the x coordinate of the output location we're focusing on
+    	last_in_row = pad_index + data_size_X;
+		for(; pad_index < last_in_row; pad_index++){ // the y coordinate of theoutput location we're focusing on
+				out[out_index] = flipped_kernel[0] * padded_in[pad_index - 1 - padded_size_X];
+				out[out_index] += flipped_kernel[1] * padded_in[pad_index - padded_size_X];
+				out[out_index] += flipped_kernel[2] * padded_in[pad_index + 1 - padded_size_X];
+				out[out_index] += flipped_kernel[3] * padded_in[pad_index - 1];
+				out[out_index] += flipped_kernel[4] * padded_in[pad_index];
+				out[out_index] += flipped_kernel[5] * padded_in[pad_index +1];
+				out[out_index] += flipped_kernel[6] * padded_in[pad_index - 1 + padded_size_X];
+				out[out_index] += flipped_kernel[7] * padded_in[pad_index + padded_size_X];
+				out[out_index] += flipped_kernel[8] * padded_in[pad_index + 1 + padded_size_X];
+			out_index++;
     	}
-		for(; x < data_size_X; x++){
-			in_location = padded_in + pad_amount_X + x - 1 + (pad_amount_Y + y - 1) * padded_size_X;
-			matrix_row = _mm_loadu_ps(in_location);
-			res_row = _mm_dp_ps(matrix_row, kernel_row1, dp_mask);
-			sum_res1 = res_row;
-			
-			in_location += padded_size_X;	
-			matrix_row = _mm_loadu_ps(in_location);
-			res_row = _mm_dp_ps(matrix_row, kernel_row2, dp_mask);
-			sum_res1 = _mm_add_ps(res_row, sum_res1);
-			
-			in_location += padded_size_X;	
-			matrix_row = _mm_loadu_ps(in_location);
-			res_row = _mm_dp_ps(matrix_row, kernel_row3, dp_mask);
-			sum_res1 = _mm_add_ps(res_row, sum_res1);
-			
-			_mm_storeu_ps(sum, sum_res1);
-			out[out_location++] = sum[0];
-		}	
-		x = 0; 
-   	}
-	free(padded_in);
+	}
+
+	free(padded_in);	
 	return 1;
 }
